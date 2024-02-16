807d0d30f874ed475451a62341834f361fb0f189
Saurabh Nagorao Radke <111894486+SaurabhRadke@users.noreply.github.com>
2024-02-17 01:33:20 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,17 +10,11 @@ import LandingPage from "./Components/LandingPage";
 import LightGreen from "./Components/LightGreen";
 import Navbar from "./Components/Navbar";
 import ReadyStart from "./Components/ReadytoStart";
-import LocomotiveScroll from 'locomotive-scroll';
 import { useEffect, useState } from "react";
 import HiddenComponent from "./Components/TopLoadingPage";
 export default function Home() {
   const [blurr, setBlurr] = useState(true)
   // const locomotiveScroll = new LocomotiveScroll();
-  useEffect(() => {
-    // Check if window is defined (i.e., running in a browser environment)
-    if (typeof window !== 'undefined') {
-      const locomotiveScroll = new LocomotiveScroll();}
-  }, []);
 
   return (
 
